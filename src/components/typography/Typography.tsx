31304a3daa676215c424febd091bc18d723b0dd1
carrein <carreinlee@proton.me>
2025-03-10 03:06:18 +0800

    update typography

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 const BaseTypography = css`
   font-weight: normal;
   margin: 0;
-  color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Title = styled.h1`
@@ -11,7 +11,7 @@ export const Title = styled.h1`
   font-size: 4rem;
 `;
 
-export const Paragraph = styled.span`
+export const Subtitle = styled.span`
   ${BaseTypography}
   font-size: 2rem;
 `;
@@ -24,4 +24,11 @@ export const Petite = styled.span`
 export const Tiny = styled.span`
   ${BaseTypography}
   font-size: 1rem;
+`;
+
+export const Label = styled.label`
+  ${BaseTypography}
+  font-size: 1.2rem;
+  font-weight: 700;
+  letter-spacing: 1.2px;
 `;
